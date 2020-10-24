89d3d2bd0989499dd9ffa427e3340f771013e1ab
byron7cueva <byron7cueva@gmail.com>
2020-10-24 10:28:48 -0500

    Checkbox and radio

--- platzi-store/src/app/admin/components/basic-form/basic-form.component.ts
+++ platzi-store/src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+interface Preference {
+  area: string;
+  value: string;
+}
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+interface GenericEvent<T> extends Omit<Event, 'target'> {
+    target: T;
+}
+
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
@@ -18,7 +29,27 @@ export class BasicFormComponent implements OnInit {
   urlField = new FormControl('');
   rangeField = new FormControl(5);
   timeField = new FormControl('');
+  searchFile = new FormControl('');
   descriptionField = new FormControl('');
+
+  categoryField = new FormControl('category-3');
+  // El value va estar separado por comas
+  tagField = new FormControl('');
+
+  agreeField = new FormControl(false);
+  genderField = new FormControl('');
+  zoneField = new FormControl('');
+  preferences: string[] = [];
+
+  preferencesList: Array<Preference> = [
+    { area: 'Contenido Digital', value: 'contenido-digital' },
+    { area: 'Desarrollo e Ingeniería', value: 'desarrollo' },
+    { area: 'Diseño y UX', value: 'diseño' },
+    { area: 'Marketing', value: 'marketing' },
+    { area: 'Negocion y Emprendimiento', value: 'negocios' },
+    { area: 'Crecimiento Profesional', value: 'crecimiento-profesional' },
+    { area: 'Startup', value: 'startup' }
+  ];
 
   constructor() { }
 
@@ -35,6 +66,16 @@ export class BasicFormComponent implements OnInit {
   getNameValue() {
     // Ver el valor del field
     console.log(this.nameField.value);
+  }
+
+  checkboxChangeHandle(event: GenericEvent<HTMLInputElement>) {
+    const { target: { checked, value } } = event;
+    if (checked) {
+      this.preferences.push(value);
+    } else {
+      const index = this.preferences.indexOf(value);
+      this.preferences.splice(index, 1);
+    }
   }
 
 }
