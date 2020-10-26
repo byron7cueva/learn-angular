d166dc3059bcbd6a524ce7a479ae1819b2158700
byron7cueva <byron7cueva@gmail.com>
2020-10-26 05:28:55 -0500

    Categories list

--- platzi-store/src/app/admin/categories/components/categories/categories.component.ts
+++ platzi-store/src/app/admin/categories/components/categories/categories.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '@core/models/category.model';
+import { CategoriesService } from '@core/services/categories.service';
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  displayColumns: string[] = ['name', 'image'];
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.categoriesService.getAllCategories()
+    .subscribe(categories => {
+      this.categories = categories;
+    });
   }
 
 }
